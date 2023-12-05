af9459239a8726bd0aad01fed1e6daf429d464d2
Leo <leleko-kaka@hotmail.com>
2023-12-05 13:53:03 -0300

    Adicionado programacao com os testes aprovados

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    let firstChose= "Pedra"
+    let secondChose= "Papel"
+    let thirdChose= "Tesoura"
+    
+    if (player1== firstChose && player2 == thirdChose){
+        return "Jogador 1 venceu!"
+    }
+    else if (player1== firstChose && player2 == secondChose){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1== secondChose && player2 == firstChose){
+        return "Jogador 1 venceu!"
+    }
+    else if (player1== secondChose && player2 == thirdChose){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1== thirdChose && player2 == firstChose){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1== thirdChose && player2== secondChose){
+        return "Jogador 1 venceu!"
+    }
+    return "Empate!"
+    }
+        
