cbe1e8ceac6fce83314f27233197e1cb94f66782
Leo <103613540+ticooh@users.noreply.github.com>
2023-12-12 15:07:35 -0300

    atualizacao simplificada com uso do ||

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-function playRockPaperScissor(player1, player2){
-    let firstChose= "Pedra"
-    let secondChose= "Papel"
-    let thirdChose= "Tesoura"
-    
-    if (player1== firstChose && player2 == thirdChose){
-        return "Jogador 1 venceu!"
+function playRockPaperScissor(player1, player2) {
+    if ((player1 === "rock" && player2 === "scissor") ||
+        (player1 === "paper" && player2 === "rock") ||
+        (player1 === "scissor" && player2 === "paper")) {
+        return "Jogador 1 venceu!";
+    } else if ((player2 === "rock" && player1 === "scissor") ||
+               (player2 === "paper" && player1 === "rock") ||
+               (player2 === "scissor" && player1 === "paper")) {
+        return "Jogador 2 venceu!";
     }
-    else if (player1== firstChose && player2 == secondChose){
-        return "Jogador 2 venceu!"
-    }
-    else if (player1== secondChose && player2 == firstChose){
-        return "Jogador 1 venceu!"
-    }
-    else if (player1== secondChose && player2 == thirdChose){
-        return "Jogador 2 venceu!"
-    }
-    else if (player1== thirdChose && player2 == firstChose){
-        return "Jogador 2 venceu!"
-    }
-    else if (player1== thirdChose && player2== secondChose){
-        return "Jogador 1 venceu!"
-    }
-    return "Empate!"
-    }
-        
+    return "Empate!";
+}
+
+console.log(playRockPaperScissor("paper", "rock"));
