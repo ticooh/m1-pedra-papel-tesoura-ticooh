a3b38d401beece933414102cc3594a03f4ce2163
Leo <103613540+ticooh@users.noreply.github.com>
2023-12-12 17:36:05 -0300

    atualizacao para o uso de ||

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function playRockPaperScissor(player1, player2) {
-    if ((player1 === "rock" && player2 === "scissor") ||
-        (player1 === "paper" && player2 === "rock") ||
-        (player1 === "scissor" && player2 === "paper")) {
+function playRockPaperScissor(jogador1, jogador2) {
+    jogador1 = jogador1.toLowerCase();
+    jogador2 = jogador2.toLowerCase();
+
+    if ((jogador1 === "pedra" && jogador2 === "tesoura") ||
+        (jogador1 === "papel" && jogador2 === "pedra") ||
+        (jogador1 === "tesoura" && jogador2 === "papel")) {
         return "Jogador 1 venceu!";
-    } else if ((player2 === "rock" && player1 === "scissor") ||
-               (player2 === "paper" && player1 === "rock") ||
-               (player2 === "scissor" && player1 === "paper")) {
+    } else if ((jogador2 === "pedra" && jogador1 === "tesoura") ||
+               (jogador2 === "papel" && jogador1 === "pedra") ||
+               (jogador2 === "tesoura" && jogador1 === "papel")) {
         return "Jogador 2 venceu!";
     }
     return "Empate!";
 }
 
-console.log(playRockPaperScissor("paper", "rock"));
+console.log(playRockPaperScissor("PAPEL", "pedra"));
